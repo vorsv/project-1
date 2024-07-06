09c3195adb98bc747f8935f1ea31e1f62a7debfa
vorsv <66461662+vorsv@users.noreply.github.com>
2024-07-06 06:11:53 +0530

    added .env app.js and db.json to auth erver

---
+++ TWOO/auth-server/App.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var cors = require('cors')
+var bcrypt = require('bcrypt')
+var jwt = require('jsonwebtoken')
+var low = require('lowdb')
+var FileSync = require('lowdb/adapters/FileSync')
+
+var adapter = new FileSync('./db.json')
+var db = low(adapter)
+
+const app = express()
+
+const jwtSecret = {process.env.REACT_APP_CODE}
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
