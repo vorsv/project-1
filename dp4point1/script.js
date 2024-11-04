7fc99fc4b25f7ad451236c255532a0b975006f26
vorsv <66461662+vorsv@users.noreply.github.com>
2024-11-04 08:00:39 +0530

    updaaated working with text matcher

--- dp4point1/script.js
+++ dp4point1/script.js
@@ -1,28 +1,13 @@
-function createList(spacecrafts){
-
-    var listView=document.createElement('ol');
-    
-    for(var i=0;i<spacecrafts.length;i++)
-    {
-        var listViewItem=document.createElement('li');
-        listViewItem.appendChild(document.createTextNode(spacecrafts[i]));
-        listView.appendChild(listViewItem);
-    }
-    
-    return listView;
-}
-
 function checkSpam() {
     const emailContent = document.getElementById('emailContent').value;
     const tokens = emailContent.toLowerCase().split(/\s+/);
-    const spamKeywords = ['win', 'free', 'prize', 'click', 'claim', 'urgent','unsubscribe'];
-    const found = []
+    const spamKeywords = ['win', 'free', 'prize', 'click', 'claim', 'urgent'];
     let isSpam = false;
 
     for (let token of tokens) {
         if (spamKeywords.includes(token)) {
             isSpam = true;
-            found.push(token);
+            break;
         }
     }
 
@@ -34,5 +19,4 @@ function checkSpam() {
         resultElement.textContent = 'The email seems legitimate.';
         resultElement.style.color = 'green';
     }
-    document.getElementById("ol").appendChild(createList(found));
 }
